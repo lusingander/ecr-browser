d000b6cd8c5d9889b8604189d449d26f576edc80
Kyosuke Fujimoto <kyoro.f@gmail.com>
2020-08-25 08:51:43 +0900

    Add mock flag

--- main.go
+++ main.go
@@ -1,16 +1,35 @@
 package main
 
 import (
+	"flag"
+	"log"
+
 	"github.com/lusingander/ecr-browser/aws"
 	"github.com/lusingander/ecr-browser/domain"
+	"github.com/lusingander/ecr-browser/mock"
 	"github.com/lusingander/ecr-browser/ui"
 )
 
+var (
+	useMock *bool
+)
+
+func parseFlags() {
+	useMock = flag.Bool("mock", false, "Use mock data")
+	flag.Parse()
+}
+
 func newClient() domain.ContainerClient {
+	if *useMock {
+		return mock.NewMockClient()
+	}
 	return aws.NewAwsEcrClient()
 }
 
 func main() {
+	parseFlags()
 	cli := newClient()
-	ui.Start(cli)
+	if err := ui.Start(cli); err != nil {
+		log.Fatal(err)
+	}
 }
