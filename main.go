c5fe5fccaed3f3ac61d602aa8c6df8950fac1a33
Kyosuke Fujimoto <kyoro.f@gmail.com>
2020-08-25 08:42:26 +0900

    Implement mock repository

--- main.go
+++ main.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/lusingander/ecr-browser/aws"
+	"github.com/lusingander/ecr-browser/domain"
 	"github.com/lusingander/ecr-browser/ui"
 )
 
+func newClient() domain.ContainerClient {
+	return aws.NewAwsEcrClient()
+}
+
 func main() {
-	cli := aws.NewAwsEcrClient()
+	cli := newClient()
 	ui.Start(cli)
 }
---
+++ mock/mock.go
@@ -0,0 +1,62 @@
+package mock
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"strconv"
+	"time"
+
+	"github.com/lusingander/ecr-browser/domain"
+)
+
+type mockClinet struct{}
+
+func NewMockClient() domain.ContainerClient {
+	return &mockClinet{}
+}
+
+func (*mockClinet) FetchAllRepositories() ([]*domain.Repository, error) {
+	n := 20
+	repos := make([]*domain.Repository, 0, n)
+	for i := 1; i <= n; i++ {
+		repos = append(repos, repo(i))
+	}
+	return repos, nil
+}
+
+func (*mockClinet) FetchAllImages(repo string) ([]*domain.Image, error) {
+	time.Sleep(time.Second)
+
+	n := 50
+	images := make([]*domain.Image, 0, n)
+	for i := 1; i <= n; i++ {
+		images = append(images, image(i, repo))
+	}
+	return images, nil
+}
+
+func repo(i int) *domain.Repository {
+	name := fmt.Sprintf("sample-repo-%02d", i)
+	uri := fmt.Sprintf("xxx.dkr.ecr.ap-northeast-1.amazonaws.com/%s", name)
+	arn := fmt.Sprintf("arn:aws:ecr:ap-northeast-1:xxx:repository/%s", name)
+	tagMutability := "MUTABLE"
+	createdAt := time.Now().AddDate(0, 0, i)
+	return domain.NewRepository(name, uri, arn, tagMutability, createdAt)
+}
+
+func image(i int, repo string) *domain.Image {
+	is := strconv.Itoa(i)
+	commit := fmt.Sprintf("%x", sha256.Sum256([]byte(is)))[:7]
+	var tags []string
+	if i == 1 {
+		tags = []string{commit, "latest"}
+	} else if i%10 == 0 {
+		tags = []string{}
+	} else {
+		tags = []string{commit}
+	}
+	pushedAt := time.Now().AddDate(0, 0, -i)
+	digest := fmt.Sprintf("sha256:%x", sha256.Sum256([]byte(repo+is)))
+	sizeByte := 1024 * 1024 * i / 2
+	return domain.NewImage(tags, pushedAt, digest, int64(sizeByte))
+}
